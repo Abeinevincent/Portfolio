bab82d55b807b99da515f030d1ab22ee311628b7
AbeinemukamaVicent <48617563+Abeinevincent@users.noreply.github.com>
2021-11-05 09:49:36 +0300

    Update web.js

--- web.js
+++ web.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Front-end Developer", "Back-end Developer", "Designer", "Photo Editor", "Freelancer"],
+        strings: ["Mern Stack Developer", "Mobile Developer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Front-end Developer", "Back-end Developer", "Designer", "Photo Editor", "Freelancer"],
+        strings: ["Mern Stack Developer", "Mobile Developer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
